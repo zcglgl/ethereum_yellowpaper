ac3bad400285612dd23be2605b67cbdce33c55ae
Rivers Yang <rivers.yang@icloud.com>
2018-05-04 17:09:12 +0800

    Update shell, bib and config contents.

--- build_ethereum_yellow_paper_cn.sh
+++ build_ethereum_yellow_paper_cn.sh
@@ -2,15 +2,23 @@
 
 set -e
 
+rm -rf Options.tex
+
 if [ -d ".git" ]; then
 
 SHA=`git rev-parse --short --verify HEAD`
 DATE=`git show -s --format="%cd" --date=short HEAD`
-REV="$SHA - $DATE"
+# REV="$SHA - $DATE"
+# chinese version's origin version
+REV="47d2826 - 2018-04-26"
+echo "\def\YellowPaperVersionNumber{$REV}" >> Options.tex
 
-else
+fi
 
-REV="unknown revision"
+
+if [ "$1" == "white" ]; then
+
+echo "\definecolor{pagecolor}{rgb}{1,1,1}" >> Options.tex
 
 fi
 
@@ -21,5 +29,5 @@ xelatex -interaction=errorstopmode -halt-on-error ethereum_yellow_paper_cn.tex &
 bibtex ethereum_yellow_paper_cn && \
 xelatex -interaction=errorstopmode -halt-on-error ethereum_yellow_paper_cn.tex && \
 xelatex -interaction=errorstopmode -halt-on-error ethereum_yellow_paper_cn.tex && \
-xelatex -interaction=errorstopmode -halt-on-error ethereum_yellow_paper_cn.tex
-
+xelatex -interaction=errorstopmode -halt-on-error ethereum_yellow_paper_cn.tex && \
+rm -rf Options.tex
